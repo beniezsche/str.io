6d35a8f1500f43474fea105c19b83e502d3458af
bikraman <bikramankarmakar10@gmail.com>
2024-04-21 02:11:52 +0530

    VideoDecoder code written

--- script.js
+++ script.js
@@ -106,23 +106,60 @@ videoReader.addEventListener('load', (event) => {
   // video.width = documentWidth;
   // video.height = documentHeight;
 
-  video.src = url;
-  video.play();
+  // video.src = url;
+  // video.play();
 
-  console.log("hello")
 
   // new VideoContainer(0,0,)
+
+    // Check if the browser supports the WebCodecs API
+  if ('VideoDecoder' in window) {
+
+    console.log("videodecoder exists");
+    // Create a new VideoDecoder instance
+    const videoDecoder = new VideoDecoder({
+        output: (frame) => {
+            // This callback function is called whenever a video frame is decoded
+            // 'frame' is a VideoFrame object representing the decoded frame
+            // You can render the frame onto a canvas, display it in a video element, etc.
+            console.log(frame)
+            context.drawImage(frame, 0, 0, canvas.width, canvas.height);
+        }
+    });
+
+    try {
+      videoDecoder.decode(buffer);
+    }
+    catch (e) {
+      console.log(e)
+    }
+
+
+    // Fetch and decode a video file
+    // fetch('example-video.mp4')
+    //     .then(response => response.arrayBuffer())
+    //     .then(arrayBuffer => {
+    //         // Decode the video data
+    //         videoDecoder.decode(arrayBuffer);
+    //     })
+    //     .catch(error => {
+    //         console.error('Error fetching or decoding video:', error);
+    //     });
+  } else {
+    console.error('WebCodecs API is not supported in this browser.');
+  }
+
   
 });
 
-video.addEventListener("play", (event) => {
-  console.log(event)
-  function step() {
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    requestAnimationFrame(step);
-  }
-  requestAnimationFrame(step);
-})
+// video.addEventListener("play", (event) => {
+//   console.log(event)
+//   function step() {
+//     context.drawImage(video, 0, 0, canvas.width, canvas.height);
+//     requestAnimationFrame(step);
+//   }
+//   requestAnimationFrame(step);
+// })
 
 addTextButton?.addEventListener('click', (event) => {
   submitText();
